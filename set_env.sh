678a4e3c4b0f079341d10ce9ae7c3a704824c954
quochd <huynhducquoc1995@gmail.com>
2024-09-25 21:19:43 +0700

    add file5

--- set_env.sh
+++ set_env.sh
@@ -8,4 +8,4 @@ export AWS_BUCKET=myudacity-610844210820-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
-export URL=http://a9b956608a89340fe97ff4782604e632-673776864.us-east-1.elb.amazonaws.com:8100
+export URL=http://aba06b0f124b4419fba6f9058551ca51-1829423399.us-east-1.elb.amazonaws.com:8100
